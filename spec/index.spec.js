195be2754d07bd92784cb99380f9c918c39da05a
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-03 10:09:04 -0600

    Move Food tests to own folder and change index test back

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,70 +1,7 @@
-var shell = require('shelljs');
-var request = require("supertest");
-var app = require('../app');
-const pry = require('pryjs');
-
-describe('api', () => {
-  describe('Food', () => {
-    beforeEach((done) => {
-      server = app.listen(4000, (err) => {
-        if (err) return done(err);
-
-        agent = request.agent(server);
-        done();
-      });
-      shell.exec('npx sequelize db:migrate')
-      shell.exec('npx sequelize db:seed:all')
-    });
-    afterEach((done) => {
-      shell.exec('npx sequelize db:migrate:undo:all')
-      return  server && server.close(done);
-    });
-
-    describe('food index page', () => {
-      it('loads food items successfully', async () => {
-        const response = await agent.get('/api/v1/foods');
-        expect(response.statusCode).toBe(200)
-        expect(response.body.length).toEqual(4)
-        expect(Object.keys(response.body[0])).toContain('name')
-        expect(Object.keys(response.body[0])).toContain('calories')
-      })
-    });
-  });
-  describe('Sad Path', () => {
-    beforeEach((done) => {
-      server = app.listen(4000, (err) => {
-        if (err) return done(err);
-
-        agent = request.agent(server);
-        done();
-      });
-    });
-    afterEach((done) => {
-      return  server && server.close(done);
-    });
-    describe('food index page', () => {
-      it('loads food items unsuccessfully', async () => {
-        const response = await agent.get('/api/v1/foods');
-        expect(response.statusCode).toBe(500)
-      });
-    });
-  });
-});
-
 describe('index page', () => {
-  beforeEach((done) => {
-    server = app.listen(4000, (err) => {
-      if (err) return done(err);
-
-      agent = request.agent(server);
-      done();
-    });
+  it('loads successfully', () => {
+    return get("/").then(response => {
+      expect(response.statusCode).toBe(200)
+    })
   });
-  afterEach((done) => {
-    return  server && server.close(done);
-  });
-  it('loads successfully', async () => {
-    const response = await agent.get("/");
-    expect(response.statusCode).toBe(200);
-  });
-});
+})
