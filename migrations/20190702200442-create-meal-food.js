124d9562833959d6b0df757a0ed7a6aa193558c0
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-02 21:36:01 -0600

    Rectify pluralization issue

--- migrations/20190702200442-create-meal-food.js
+++ migrations/20190702200442-create-meal-food.js
@@ -18,7 +18,7 @@ module.exports = {
       FoodId: {
         type: Sequelize.INTEGER,
         references: {
-          model: 'Foods',
+          model: 'Food',
           key: 'id'
         }
       },
