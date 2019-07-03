1147167958eebb57653ffef08f7715cf487fcca1
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-03 15:27:40 -0600

    Add meal/show endpoint

--- routes/api/v1/meals.js
+++ routes/api/v1/meals.js
@@ -21,4 +21,26 @@ meals.get('/', (req, res, next) => {
   })
 })
 
+meals.get('/:id', (req, res, next) => {
+  res.setHeader("Content-Type", "application/json")
+  Meal.findByPk(req.params.id, {
+    attributes: ["id", "name"],
+    include: [{
+      model: Food,
+      attributes: ["id", "name", "calories"],
+      through: {
+        attributes: []
+      }
+    }]
+  }).then(meal => {
+    if(meal){
+      res.status(200).send(meal)
+    } else {
+      res.status(404).send({error: "No meal found with the provided ID."})
+    }
+  }).catch(error => {
+    res.status(500).send({error: "Internal Server Error"})
+  })
+})
+
 module.exports = meals
