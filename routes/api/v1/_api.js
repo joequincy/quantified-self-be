efa181701bd9f125f280d8e62da64c1ca6186510
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-03 10:11:15 -0600

    Add Food route for index endpoint

--- routes/api/v1/_api.js
+++ routes/api/v1/_api.js
@@ -1,5 +1,7 @@
 var api = require('express').Router();
 var mealsRouter = require('./meals');
+var foodsRouter = require('./foods');
 
 api.use('/meals', mealsRouter);
+api.use('/foods', foodsRouter);
 module.exports = api;
