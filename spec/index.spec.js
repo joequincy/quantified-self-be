a8f3d1f9af92fa14e7bbe27ca3844944be241655
Deonte Cooper <deonte.cooper12@gmail.com>
2019-07-02 19:52:54 -0600

    Add test and sad path test for Food Index Endpoint

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,7 +1,60 @@
+var shell = require('shelljs');
+var request = require("supertest");
+var app = require('../app');
+const pry = require('pryjs');
+
+describe('api', () => {
+  describe('Food', () => {
+    beforeEach((done) => {
+      server = app.listen(4000, (err) => {
+        if (err) return done(err);
+
+        agent = request.agent(server);
+        done();
+      });
+      shell.exec('npx sequelize db:migrate')
+      shell.exec('npx sequelize db:seed:all')
+    });
+    afterEach((done) => {
+      shell.exec('npx sequelize db:migrate:undo:all')
+      return  server && server.close(done);
+    });
+
+    describe('food index page', () => {
+      it('loads food items successfully', async () => {
+        const response = await agent.get('/api/v1/foods');
+        expect(response.statusCode).toBe(200)
+        expect(response.body.length).toEqual(4)
+        expect(Object.keys(response.body[0])).toContain('name')
+        expect(Object.keys(response.body[0])).toContain('calories')
+      })
+    });
+  })
+  describe('Sad Path', () => {
+    describe('food index page', () => {
+      it('loads food items unsuccessfully', async () => {
+        const response = await agent.get('/api/v1/foods');
+        // .set('Content-Type', 'application/json')
+        expect(response.statusCode).toBe(500)
+      });
+    });
+  });
+});
+
 describe('index page', () => {
-  it('loads successfully', () => {
-    return get("/").then(response => {
-      expect(response.statusCode).toBe(200)
-    })
+  beforeEach((done) => {
+    server = app.listen(4000, (err) => {
+      if (err) return done(err);
+
+      agent = request.agent(server);
+      done();
+    });
+  });
+  afterEach((done) => {
+    return  server && server.close(done);
+  });
+  it('loads successfully', async () => {
+    const response = await agent.get("/");
+    expect(response.statusCode).toBe(200);
   });
 })
